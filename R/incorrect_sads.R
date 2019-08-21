29ad216bb9375a3ce7cd0f07222d1f71c8ad6599
Renata Diaz <diaz.renatam@gmail.com>
2019-08-21 17:03:19 -0400

    new fake sad functions

---
+++ R/incorrect_sads.R
@@ -0,0 +1,60 @@
+#' Generate an even SAD
+#'
+#' Generate the most even possible SAD of S species summing to N 
+#' @param sad_to_use sad
+#'
+#' @return even sad
+#' @export
+#'
+generate_even_sad <- function(sad_to_use) {
+  even_sad <- rep(floor(sum(sad_to_use) / length(sad_to_use)), times = length(sad_to_use))
+  remainder <- sum(sad_to_use) - sum(even_sad)
+  even_sad[1:remainder] <- even_sad[1:remainder] + 1
+  return(sort(even_sad))
+}
+
+#' Generate stepwise sad
+#'
+#' @param sad_to_use sad
+#'
+#' @return stepwise increasing sad
+#' @export
+#'
+generate_stepwise_sad <- function(sad_to_use) {
+  
+  nsteps <- sum(1:length(sad_to_use))
+  
+  step_size <- sum(sad_to_use) / nsteps
+  
+  stepwise_sad <- as.integer(1:length(sad_to_use) * step_size)
+  
+  remainder <- sum(sad_to_use) - sum(stepwise_sad)
+  
+  stepwise_sad[1:remainder] <- stepwise_sad[1:remainder] + 1
+  
+  return(sort(stepwise_sad))
+}
+
+#' Generate exponential SAD
+#'
+#' @param sad_to_use sad
+#'
+#' @return exponential-like SAD
+#' @export
+#'
+generate_exponential_sad <- function(sad_to_use) {
+  
+  powers <- c(0:(length(sad_to_use) -1))  
+  
+  coeffs <- 2^powers
+  
+  units <- sum(sad_to_use) / sum(coeffs)
+  
+  exp_sad <- as.integer(coeffs * units)
+  
+  remainder <- sum(sad_to_use) - sum(exp_sad)
+  
+  exp_sad[1:remainder] <- exp_sad[1:remainder] + 1
+  
+  return(exp_sad)
+}
