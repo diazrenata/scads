f8cfcdbf84f83bfdeef8b024f59a13e7502b8132
Renata Diaz <diaz.renatam@gmail.com>
2019-09-01 17:36:02 -0400

    change old faithful to portal data

--- legendre-vis/app.R
+++ legendre-vis/app.R
@@ -8,12 +8,13 @@
 #
 
 library(shiny)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Just an SAD"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
@@ -35,9 +36,18 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
+    portal_data <- isds::get_toy_portal_data(download=T, years = c(1990:1995))
+    portal_sad <- portal_data %>%
+      select(species) %>%
+      group_by(species) %>%
+      summarize(abund = n()) %>%
+      ungroup() %>%
+      select(abund) %>%
+      arrange(abund)
+    
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
+      x    <- portal_sad$abund 
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
       # draw the histogram with the specified number of bins
