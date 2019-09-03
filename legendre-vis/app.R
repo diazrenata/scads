1ab7189b5f4a1e6fdc34248957af128e52d6f2c0
Renata Diaz <diaz.renatam@gmail.com>
2019-09-03 13:41:32 -0400

    add comparison schemes to coefficients

--- legendre-vis/app.R
+++ legendre-vis/app.R
@@ -13,72 +13,74 @@ library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Just an SAD"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30),
-         numericInput("nb_leg", "Number of polynomials",
-                      min = 2, max = 10, step = 1, value = 3)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot"),
-         plotOutput("legestPlot")
-      )
-   )
+  
+  # Application title
+  titlePanel("Just an SAD"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      textInput("abund",
+                "Abundance vector:",
+                value = "1,1,7,36,69,89,99,174,308,1616"),
+      numericInput("nb_leg", "Number of polynomials",
+                   min = 2, max = 10, step = 1, value = 3)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("legestPlot")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-    portal_data <- isds::get_toy_portal_data(download=T, years = c(1990:1995))
-    portal_sad <- portal_data %>%
-      select(species) %>%
-      group_by(species) %>%
-      summarize(abund = n()) %>%
-      ungroup() %>%
-      select(abund) %>%
-      arrange(abund)
+  output$legestPlot <- renderPlot({
+    abund <- as.integer(unlist(strsplit(input$abund, ",")))
+    
+    sad_df <- list(
+      provided = abund,
+      mete_draw = scads::sample_METE(s = length(abund), n = sum(abund), nsamples = 1)$V1,
+      poilog_draw = scads::draw_poilog_samples(abund),
+      #fs_draw = scads::sample_feasibleset(s = length(abund), n = sum(abund), nsamples = 1)$V1,
+      flat = scads::generate_even_sad(abund),
+      stepwise = scads::generate_stepwise_sad(abund),
+      expon = scads::generate_exponential_sad(abund),
+      prec = scads::generate_precipice_sad(abund)
+    ) %>%
+      bind_rows() %>%
+      mutate(rank = row_number()) %>%
+      tidyr::gather(-rank, key = "source", value = "abund")
+    
+    sads_plot <- ggplot(data = sad_df, aes(x = rank, value = abund, color = source)) +
+      geom_line() +
+      theme_bw()
+    
+    sads_plot
     
     
+    leg_coeffs <- lapply(as.list(unique(sad_df$source)),
+                         FUN = function(source_name) 
+                           return(data.frame(coeff = scads::legendre_approx(filter(sad_df, source == source_name)$abund, 
+                                                                     nleg = input$nb_leg)$coefficients,
+                                             coeff_name = names(scads::legendre_approx(filter(sad_df, source == source_name)$abund, 
+                                                                                nleg = input$nb_leg)$coefficients),
+                                             source = source_name, stringsAsFactors = F)))
     
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- portal_sad$abund 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    leg_coeffs <- bind_rows(leg_coeffs)
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   }, width = 300, height = 300)
-   
-   output$legestPlot <- renderPlot({
-     
-     leg_estimate <- scads::legendre_approx(portal_sad$abund, nleg = input$nb_leg)
-     
-     leg_coeffs <- data.frame(
-       coeff_name = names(leg_estimate$coefficients),
-       coeff = leg_estimate$coefficients
-     )
-     
-     sse <- sum(leg_estimate$residuals ^ 2)
-     
-     legplot <- ggplot(data = leg_coeffs, aes(x = coeff_name, y = coeff)) +
-       geom_point() +
-       theme_bw() +
-       ggtitle(paste0("Sum squared error: ", sse))
-     
-     legplot
-     
-   })
+    
+    
+    legplot <- ggplot(data = leg_coeffs, aes(x = coeff_name, y = coeff, color = source)) +
+      geom_point() +
+      theme_bw() +
+      facet_wrap(source ~ .) +
+      theme(legend.position = "none")
+    
+    legplot
+    
+  })
 }
 
 # Run the application 
