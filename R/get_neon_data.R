2511da292575bcc66817fd40fea742ed50209d0d
Renata Diaz <diaz.renatam@gmail.com>
2019-10-12 17:55:48 -0400

    starting to import NEON data

---
+++ R/get_neon_data.R
@@ -0,0 +1,47 @@
+#' Download NEON data
+#'
+#' Download raw data using neonUtilities.
+#'
+#' @parameter taxonType "aquatic_plants", etc
+#'
+#' @export
+#' @importFrom neonUtilities zipsByProduct stackByTable
+#' @importFrom here here
+download_neon_data <- function(taxonType){
+  
+  save_filepath <- here::here("reports", "datasets", "neon", taxonType)
+  
+  if(!dir.exists(here::here("reports", "datasets", "neon", taxonType))) {
+    dir.create(save_filepath)
+  }
+  
+  if(taxonType == "aquatic_plants") {
+    dataProductID <- "DP1.20072.001"
+  }
+  
+  neonUtilities::zipsByProduct(dpID = dataProductID, site = "all",
+                               savepath = save_filepath)
+  
+
+  filepath_forstack <- file.path(save_filepath, list.dirs(save_filepath, full.names = FALSE)[ which(nchar(list.dirs(save_filepath, full.names = FALSE)) > 2)])
+  
+  neonUtilities::stackByTable(filepath = filepath_forstack, folder = T)
+}
+#' 
+#' #' Load NEON data
+#' #' 
+#' #' Load NEON data from downloaded and stacked tables.
+#' #' @parameter taxonType "aquatic_plants", etc
+#' #' 
+#' #' @export
+#' load_neon_data <- function(taxonType){
+#'   
+#'   if(taxonType == "aquatic_plants") {
+#'     
+#'     
+#'     
+#'     
+#'   }
+#'   
+#'   
+#' }
